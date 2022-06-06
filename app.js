e0f39d0c18639f6524250781e0e09799bdc6bb54
Prankur Gupta <prankurgupta@Prankurs-MacBook-Pro.local>
2022-06-07 01:22:58 +0530

    completly functional blog website

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const express = require("express");
 const ejs = require("ejs");
+const lodash = require("lodash");
+const { lowerCase } = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
+let posts = [];
 
 app.get("/", function (req, res) {
   res.render("home", {
@@ -38,6 +40,29 @@ app.get("/contact", function (req, res) {
 app.get("/compose", function (req, res) {
   res.render("compose");
 });
+
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = req.params.postName;
+  console.log(requestedTitle);
+  posts.forEach(post => {
+    const storedTitle = post.title;
+    if (lowerCase(storedTitle) === lowerCase(requestedTitle)){
+      res.render("post", {
+        postTitle: post.title,
+        postBody: post.body
+      });
+    }
+  });
+});
+
+
+
+
+
+
+
+
+//post the form data from the wesite
 
 app.post("/compose", function (req, res) {
   //console.log(req.body.newTitle, req.body.newPost);
